1f783c4c9e6a1d9e65580e978a6a330dca8ad92a
Alyse Ridge <94921692+aridge03@users.noreply.github.com>
2024-02-26 11:54:01 -0700

    Add the feature to share QR code and make Admin Layout

---
+++ app/src/main/java/com/example/swiftcheckin/Admin.java
@@ -0,0 +1,15 @@
+package com.example.swiftcheckin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Admin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_activity);
+    }
+
+}
--- app/src/main/java/com/example/swiftcheckin/MainActivity.java
+++ app/src/main/java/com/example/swiftcheckin/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.swiftcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.image_view);
+        Button shareImage = findViewById(R.id.btn_share);
+        Button adminButton = findViewById(R.id.btn_admin);
+        shareImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                shareImageAndText(bitmap);
+
+            }
+        });
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //switch view to admin
+                Intent intent = new Intent(MainActivity.this, Admin.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    private void shareImageAndText(Bitmap bitmap){
+        Uri uri = getImageToShare(bitmap);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_STREAM,uri);
+        intent.putExtra(Intent.EXTRA_TEXT,"Image Text");
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Image Subject");
+        intent.setType("image/*");
+        startActivity(Intent.createChooser(intent,"Share via"));
+    }
+    private Uri getImageToShare(Bitmap bitmap){
+        File folder = new File(getCacheDir(),"images");
+        Uri uri = null;
+        try{
+            folder.mkdirs();
+            File file = new File(folder, "image.jpg");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG,90,fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            uri = FileProvider.getUriForFile(this,"com.example.swiftcheckin",file);
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return uri;
+
     }
 }
