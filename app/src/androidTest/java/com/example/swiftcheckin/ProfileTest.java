36ec890db6c0154a9a1b4609c273ac1b5f5935f6
unknown <pooja_bhambri@hotmail.com>
2024-02-29 11:08:28 -0700

    Add profile activity and test

---
+++ app/src/androidTest/java/com/example/swiftcheckin/ProfileTest.java
@@ -0,0 +1,27 @@
+package com.example.swiftcheckin;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProfileTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new
+            ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void activitySwitch(){
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.profile)).check(matches(isDisplayed()));
+
+    }
+}
