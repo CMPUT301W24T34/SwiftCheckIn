4b575d8dc6fcd49d997d52e51165427aaaf0256f
unknown <pooja_bhambri@hotmail.com>
2024-02-29 23:46:26 -0700

    Change button id

--- app/src/main/java/com/example/swiftcheckin/MainActivity.java
+++ app/src/main/java/com/example/swiftcheckin/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button profileButton = findViewById(R.id.button);
+        Button profileButton = findViewById(R.id.profile_button);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
