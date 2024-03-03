b9a3b0bb1f8d4a252d6265a3bafa760704cbb16a
Alyse Ridge <94921692+aridge03@users.noreply.github.com>
2024-03-03 01:45:01 -0700

    Function to switch between admin tabs

--- app/src/main/java/com/example/swiftcheckin/Admin.java
+++ app/src/main/java/com/example/swiftcheckin/Admin.java
@@ -28,15 +28,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Admin extends AppCompatActivity {
-    ListView profilesList;
+    ListView dataList;
     Button deleteButton;
+    Button eventButton;
+    Button profileButton;
 
     ArrayList<Profile> profileList;
+    ArrayList<Event> eventList;
     private int selectedPosition = -1;
 
     final String TAG = "Sample";
     private FirebaseFirestore db;
     private CollectionReference collectionReference;
+    String tab;
+
+
 
 
     @Override
@@ -44,63 +50,94 @@ public class Admin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity);
         deleteButton = findViewById(R.id.remove_tab_button);
-        profilesList = findViewById(R.id.listView);
+        eventButton = findViewById(R.id.event_button);
+        profileButton = findViewById(R.id.profile_button);
+        dataList = findViewById(R.id.listView);
         db = FirebaseFirestore.getInstance();
-        final CollectionReference collectionReference = db.collection("profiles");
+        profileList = new ArrayList<>();
+        eventList = new ArrayList<>();
+        tab = "Event";
+        ProfileArrayAdapter profileArrayAdapter = new ProfileArrayAdapter(this, profileList);
+        EventArrayAdapter eventArrayAdapter = new EventArrayAdapter(this, eventList);
+        dataList.setAdapter(eventArrayAdapter);
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tab = "Event";
+                collectionReference = db.collection("Events");
+                dataList.setAdapter(eventArrayAdapter);
+                refreshListView( profileArrayAdapter, eventArrayAdapter,tab);
+
+            }
+        });
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tab = "Profile";
+                collectionReference = db.collection("profiles");
+                dataList.setAdapter(profileArrayAdapter);
+                refreshListView(profileArrayAdapter, eventArrayAdapter,tab);
+            }
+        });
 
 
-
-
-        profilesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        dataList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 selectedPosition = position;
             }
         });
-        profileList = new ArrayList<>();
-        ProfileArrayAdapter profileArrayAdapter = new ProfileArrayAdapter(this, profileList);
-        profilesList.setAdapter(profileArrayAdapter);
-        deleteButton.setOnClickListener(new View.OnClickListener(){
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (selectedPosition != -1) {
-                    String nameToDelete = profileList.get(selectedPosition).getName(); // Assuming 'getName()' returns the name field in your Profile class
-                    collectionReference.whereEqualTo("name", nameToDelete)
-                            .get()
-                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                    if (task.isSuccessful()) {
-                                        for (QueryDocumentSnapshot document : task.getResult()) {
-                                            String documentId = document.getId();
-                                            collectionReference.document(documentId)
-                                                    .delete()
-                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                        @Override
-                                                        public void onSuccess(Void unused) {
-                                                            Log.d(TAG, "Data has been deleted successfully!");
-                                                        }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            Log.d(TAG, "Data could not be deleted!" + e.toString());
-                                                        }
-                                                    });
+                    if  (tab == "Event") {
 
-                                            profileList.remove(selectedPosition);
-                                            profileArrayAdapter.notifyDataSetChanged();
-                                            selectedPosition = -1;
+                        }
+
+                    else if (tab == "Profile") {
+
+                        String nameToDelete = profileList.get(selectedPosition).getName(); // Assuming 'getName()' returns the name field in your Profile class
+                        collectionReference.whereEqualTo("name", nameToDelete)
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                String documentId = document.getId();
+                                                collectionReference.document(documentId)
+                                                        .delete()
+                                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                            @Override
+                                                            public void onSuccess(Void unused) {
+                                                                Log.d(TAG, "Data has been deleted successfully!");
+                                                            }
+                                                        })
+                                                        .addOnFailureListener(new OnFailureListener() {
+                                                            @Override
+                                                            public void onFailure(@NonNull Exception e) {
+                                                                Log.d(TAG, "Data could not be deleted!" + e.toString());
+                                                            }
+                                                        });
+
+                                                profileList.remove(selectedPosition);
+                                                profileArrayAdapter.notifyDataSetChanged();
+                                                selectedPosition = -1;
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Error getting documents: ", task.getException());
                                         }
-                                    } else {
-                                        Log.d(TAG, "Error getting documents: ", task.getException());
                                     }
-                                }
-                            });
+                                });
+                    }
+
                 }
             }
         });
 
+    }
+    private void refreshListView( ProfileArrayAdapter profileArrayAdapter, EventArrayAdapter eventArrayAdapter, String tab) {
         collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
@@ -109,7 +146,14 @@ public class Admin extends AppCompatActivity {
                     Log.e("Firestore", error.toString());
                     return;
                 }
-                if (queryDocumentSnapshots != null) {
+                if (tab == "Event") {
+                    eventList.clear();
+                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                        Event event = doc.toObject(Event.class);
+                        eventList.add(event);
+                    }
+                    eventArrayAdapter.notifyDataSetChanged();
+                } else if (tab == "Profile") {
                     profileList.clear();
                     for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
                         Profile profile = doc.toObject(Profile.class);
@@ -119,6 +163,7 @@ public class Admin extends AppCompatActivity {
                 }
             }
         });
+
     }
 
 
