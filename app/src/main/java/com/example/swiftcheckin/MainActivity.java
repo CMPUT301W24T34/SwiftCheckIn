d402b0537d837080c7409ae580e287d93ace5f2e
Kunaal Gupta <kunaalgupta@hotmail.com>
2024-03-04 14:15:23 -0700

    changed XML size to wrapping content

--- app/src/main/java/com/example/swiftcheckin/MainActivity.java
+++ app/src/main/java/com/example/swiftcheckin/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.attendee_myevent);
         Button profileButton = findViewById(R.id.profile_button);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
