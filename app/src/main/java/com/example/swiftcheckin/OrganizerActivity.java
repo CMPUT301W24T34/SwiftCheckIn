d72d69faed0158cdbba51b1f65bd912917e73a54
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-03-04 19:06:39 -0700

    Event Activity XML created
    New Activity page layout for event edit created

--- app/src/main/java/com/example/swiftcheckin/OrganizerActivity.java
+++ app/src/main/java/com/example/swiftcheckin/OrganizerActivity.java
@@ -34,6 +34,7 @@ public class OrganizerActivity extends AppCompatActivity implements AddEventFrag
     private ListView eventList;
     private EventArrayAdapter eventAdapter;
 
+    private String deviceId;
     private FirebaseFirestore db;
 
     Button addEventButton;
@@ -52,7 +53,7 @@ public class OrganizerActivity extends AppCompatActivity implements AddEventFrag
 
         db = FirebaseFirestore.getInstance();
 
-        String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
 //        DocumentReference deviceRef = db.collection("events").document(deviceId);
 //        DocumentReference deviceIdRef = db.collection("deviceIds").document(deviceId);
         getData();
@@ -83,7 +84,7 @@ public class OrganizerActivity extends AppCompatActivity implements AddEventFrag
     }
 
     private void getData(){
-        String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        // String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         CollectionReference eventCol = db.collection("events");
 //        DocumentReference deviceRef = eventCol.document(deviceId);
 //        CollectionReference deviceRef2 = deviceRef.collection(deviceId);
@@ -115,7 +116,7 @@ public class OrganizerActivity extends AppCompatActivity implements AddEventFrag
     }
 
     private void saveData(Event event){
-        String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        // String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         // Ensure we have 3 columns in firestore for simple reference.
         DocumentReference deviceRef = db.collection("events").document(deviceId + event.getEventTitle());
 //        CollectionReference deviceRef2 = deviceRef.collection(deviceId);
