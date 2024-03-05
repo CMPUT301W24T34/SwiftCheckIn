71d0150a1f9c937732c03953629e97a95e63b334
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-03-05 13:33:32 -0700

    Started with QR code menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-firestore")
-    implementation ("com.google.firebase:firebase-storage:19.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
