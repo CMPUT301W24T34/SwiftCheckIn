7d5b5290d95695c7f7a8895a8f1d4fab8b6ff60e
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-03-05 14:45:05 -0700

    added new layouts in menu of qr selection

--- app/src/main/java/com/example/swiftcheckin/FragmentQrcodeMenu1.java
+++ app/src/main/java/com/example/swiftcheckin/FragmentQrcodeMenu1.java
@@ -1,7 +1,22 @@
 package com.example.swiftcheckin;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class FragmentQrcodeMenu1 extends DialogFragment {
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_qrcode_choice_menu1, null);
+        Button selectQR;
+        return null;
+    }
 }
