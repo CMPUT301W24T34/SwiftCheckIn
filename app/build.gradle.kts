55b8ae86b05e1376c16a4b6d3eb2b28e26711553
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-03-05 17:44:15 -0700

    Some changes in fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.android.gms:play-services-tasks:18.1.0")
+    implementation("com.google.zxing:core:3.4.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
