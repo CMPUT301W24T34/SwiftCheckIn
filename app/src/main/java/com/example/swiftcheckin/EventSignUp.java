58e0e92c53cdf7c6d4e416d0552d78fc9f78fd22
“Prateek <prateekannam@gmail.com>
2024-03-07 14:05:43 -0700

    View Attendees

--- app/src/main/java/com/example/swiftcheckin/EventSignUp.java
+++ app/src/main/java/com/example/swiftcheckin/EventSignUp.java
@@ -31,27 +31,21 @@ public class EventSignUp {
         // How will they be able to access the things tho?
         db = FirebaseFirestore.getInstance();
         Event updated_event = createEvent(eventId);
-        HashMap<String, String> data = new HashMap<>();
-        data.put("attendee", attendeeDeviceId);
+        HashMap<String, Object> data = new HashMap<>();
+        data.put(attendeeDeviceId, attendeeDeviceId);
 
-        CollectionReference eventAttendee = db.collection("eventsWithAttendees-test");
+        CollectionReference eventAttendee = db.collection("eventsWithAttendees");
         int currentAttendees = updated_event.getCurrentAttendees();
         DocumentReference eventDoc = eventAttendee.document(eventId);
-//        if (updated_event.getMaxAttendees() == -1 || currentAttendees < updated_event.getMaxAttendees()) {
 
-        eventDoc.set(data)
+        eventDoc.update(data)
                 .addOnSuccessListener(aVoid -> {
-                    // On success
                     System.out.println("Attendee added to event successfully.");
-//                        updated_event.incrementCurrentAttendees();
                 })
                 .addOnFailureListener(e -> {
-                    // On failure
                     System.out.println("Error adding attendee to event: " + e.getMessage());
                 });
         updateEventInFirebase(updated_event);
-
-        // Need to update event
     }
 
     public Event createEvent(String eventId){
@@ -60,7 +54,6 @@ public class EventSignUp {
         final String[] eventStartTime = new String[1];
         final String[] eventStartDate = new String[1];
         final String[] eventPosterURL = new String[1];
-//        final String[] eventMaxAttendees = new String[1];
         final String[] eventLocation = new String[1];
         final String[] eventEndTime = new String[1];
         final String[] eventEndDate = new String[1];
@@ -81,7 +74,6 @@ public class EventSignUp {
                                 eventStartTime[0] = (String) event.get("eventStartTime");
                                 eventStartDate[0] = (String) event.get("eventStartDate");
                                 eventPosterURL[0] = (String) event.get("eventPosterURL");
-//                                eventMaxAttendees[0] = (String) event.get("eventMaxAttendees");
                                 eventLocation[0] = (String) event.get("eventLocation");
                                 eventEndTime[0] = (String) event.get("eventEndTime");
                                 eventEndDate[0] = (String) event.get("eventEndDate");
@@ -108,7 +100,6 @@ public class EventSignUp {
         data.put("deviceId", event.getDeviceId());
         data.put("eventDescription", event.getDescription());
         data.put("eventPosterURL", event.getEventImageUrl());
-
         data.put("eventStartDate", event.getStartDate());
         data.put("eventEndDate", event.getEndDate());
         data.put("eventStartTime", event.getStartTime());
--- app/src/main/java/com/example/swiftcheckin/ViewAttendeesActivity.java
+++ app/src/main/java/com/example/swiftcheckin/ViewAttendeesActivity.java
@@ -13,18 +13,21 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ViewAttendeesActivity extends AppCompatActivity {
 
-    ArrayList<Profile> profileList;
+    ArrayList<Profile> profileList = new ArrayList<>();
     ListView dataList;
 
     FloatingActionButton back_button;
+    ProfileArrayAdapter profileArrayAdapter;
 
     TextView bigEventTitle;
 
@@ -34,6 +37,10 @@ public class ViewAttendeesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_attendees);
         back_button = findViewById(R.id.viewAttendeeBackButton);
         bigEventTitle = findViewById(R.id.viewAttendeeEventTitle);
+        profileArrayAdapter = new ProfileArrayAdapter(this, profileList);
+        dataList = findViewById(R.id.viewAttendeeEventList);
+        dataList.setAdapter(profileArrayAdapter);
+
         String eventId = getIntent().getStringExtra("eventId");
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -62,27 +69,21 @@ public class ViewAttendeesActivity extends AppCompatActivity {
                             String eventPosterURL = (String) event.get("eventPosterURL");
                             finalUpdated_event.setEventImageUrl(eventPosterURL);
 //                                eventMaxAttendees[0] = (String) event.get("eventMaxAttendees");
-
                             String eventLocation = (String) event.get("eventLocation");
                             finalUpdated_event.setLocation(eventLocation);
-
                             String eventEndTime = (String) event.get("eventEndTime");
                             finalUpdated_event.setEndTime(eventEndTime);
-
                             String eventEndDate = (String) event.get("eventEndDate");
                             finalUpdated_event.setEndDate(eventEndDate);
-
                             String eventDescription = (String) event.get("eventDescription");
                             finalUpdated_event.setDescription(eventDescription);
-
                             String eventDeviceId = (String) event.get("deviceId");
                             finalUpdated_event.setDeviceId(eventDeviceId);
 
                         }
                     }
                 });
-        String title = finalUpdated_event.getEventTitle();
-
+        queryAttendees(eventId);
 
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,71 +93,75 @@ public class ViewAttendeesActivity extends AppCompatActivity {
         });
     }
 
-    public Event createEvent(String eventId){
-        Event updated_event = new Event();
-        final String[] eventTitle = new String[1];
-        final String[] eventStartTime = new String[1];
-        final String[] eventStartDate = new String[1];
-        final String[] eventPosterURL = new String[1];
-//        final String[] eventMaxAttendees = new String[1];
-        final String[] eventLocation = new String[1];
-        final String[] eventEndTime = new String[1];
-        final String[] eventEndDate = new String[1];
-        final String[] eventDescription = new String[1];
-        final String[] eventDeviceId = new String[1];
-
+    public void createProfile(String profileId){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference eventRef = db.collection("events").document(eventId);
-        Event finalUpdated_event = updated_event;
-        db.collection("events")
-                .document(eventId)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()){
-                            DocumentSnapshot event = task.getResult();
-                                String eventTitle  = (String) event.get("eventTitle");
-                                finalUpdated_event.setEventTitle(eventTitle);
+        DocumentReference profileRef = db.collection("profiles").document(profileId);
 
-                                String eventStartTime = (String) event.get("eventStartTime");
-                                finalUpdated_event.setStartTime(eventStartTime);
 
-                                String eventStartDate = (String) event.get("eventStartDate");
-                                finalUpdated_event.setStartDate(eventStartDate);
+        profileRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
 
-                                String eventPosterURL = (String) event.get("eventPosterURL");
-                                finalUpdated_event.setEventImageUrl(eventPosterURL);
-//                                eventMaxAttendees[0] = (String) event.get("eventMaxAttendees");
+                    if (document.exists()) {
+                        Map<String, Object> profileData = document.getData();
+                        String profileName = (String) profileData.get("name");
 
-                                String eventLocation = (String) event.get("eventLocation");
-                                finalUpdated_event.setLocation(eventLocation);
+                        String profilePhone = (String) profileData.get("phoneNumber");
+                        ;
+                        String profileAddress = (String) profileData.get("address");
+                        ;
+                        String profileLocPermission = (String) profileData.get("locationPermission");
+                        Boolean permission = Boolean.parseBoolean(profileLocPermission);
+                        ;
+                        String profileWebsite = (String) profileData.get("website");
+                        ;
+                        String profileImageUrl = (String) profileData.get("profileImageUrl");
 
-                                String eventEndTime = (String) event.get("eventEndTime");
-                                finalUpdated_event.setEndTime(eventEndTime);
+                        String profileEmail = (String) profileData.get("email");
 
-                                String eventEndDate = (String) event.get("eventEndDate");
-                                finalUpdated_event.setEndDate(eventEndDate);
+                        ;
+                        String profileBday = (String) profileData.get("birthday");
 
-                                String eventDescription = (String) event.get("eventDescription");
-                                finalUpdated_event.setDescription(eventDescription);
+                        ;
+                        profileList.add(new Profile(profileName, profileBday, profilePhone, profileEmail, profileWebsite, profileImageUrl, profileAddress, permission));
+                        profileArrayAdapter.notifyDataSetChanged();
+                        Log.e("Added Profile?", "True");
 
-                                String eventDeviceId = (String) event.get("deviceId");
-                                finalUpdated_event.setDeviceId(eventDeviceId);
-
-                        }
                     }
-                });
-
-        return finalUpdated_event;
-
+                }
+            }
+        });
     }
 
 
-    // How to retrieve data from firebase and put it into a listview?
-    // How do I even get the event id??? Done
-    // Get the Back button working Done
-    // Change the title
-            // Need to create and return event
-    // How to query through the event to get to the thing?
+
+    public void queryAttendees(String eventId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference eventRef = db.collection("eventsWithAttendees").document(eventId);
+
+        eventRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> eventData = document.getData();
+                        if (eventData != null) {
+                            for (Map.Entry<String, Object> entry : eventData.entrySet()) {
+                                String profileKey = entry.getKey();
+                                Object profileValue = entry.getValue();
+                                createProfile(profileKey); // Call your method here
+                            }
+                        }
+                    } else {
+                        Log.e("Document Doesn't Exist", "No such document");
+                    }
+                } else {
+                    Log.e("Failed Task", "Error getting documents.", task.getException());
+                }
+            }
+        });
+    }
 }
