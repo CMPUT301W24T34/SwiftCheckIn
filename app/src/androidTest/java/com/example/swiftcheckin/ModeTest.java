03635b22b6e7c8846da5b98ac6ebe7e67077d523
unknown <pooja_bhambri@hotmail.com>
2024-03-07 12:06:14 -0700

    Add javadocs to tests

--- app/src/androidTest/java/com/example/swiftcheckin/ModeTest.java
+++ app/src/androidTest/java/com/example/swiftcheckin/ModeTest.java
@@ -1,5 +1,5 @@
 package com.example.swiftcheckin;
-// This checks to see if the activity switches
+// This checks to see if the activity switches to a different mode
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -12,17 +12,25 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+/**
+ * This checks to see if the switching modes works
+ */
 public class ModeTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new
             ActivityScenarioRule<MainActivity>(MainActivity.class);
-
+    /**
+     * This checks to see if we are able to switch to admin
+     */
     @Test
     public void adminSwitch(){
         onView(withId(R.id.switch_modes)).perform(click());
         onView(withId(R.id.admin_button)).perform(click());
         onView(withId(R.id.admin)).check(matches(isDisplayed()));
     }
+    /**
+     * This checks to see if we are able to switch to organizer
+     */
     @Test
     public void organizerSwitch(){
         onView(withId(R.id.switch_modes)).perform(click());
