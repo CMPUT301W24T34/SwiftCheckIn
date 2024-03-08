cf31f569e7ebc0d6646cb03558b6994c4d61d1bf
Kunaal Gupta <kunaalgupta@hotmail.com>
2024-03-08 00:04:25 -0700

    updated tests

--- app/src/androidTest/java/com/example/swiftcheckin/MyEventActivityTest.java
+++ app/src/androidTest/java/com/example/swiftcheckin/MyEventActivityTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 /**
@@ -18,19 +20,22 @@ public class MyEventActivityTest {
     public ActivityScenarioRule<MyEventActivity> scenarioRule = new ActivityScenarioRule<>(MyEventActivity.class);
 
     /**
-     * Tests if the profile button click navigates to ProfileActivity.
+     * Tests if clicking on the profile button navigates to ProfileActivity.
      */
     @Test
     public void testProfileButton() {
         onView(withId(R.id.profile_picture)).perform(click());
+        onView(withId(R.id.profile)).check(matches(isDisplayed()));
     }
 
     /**
-     * Tests if the switch mode button click shows the switch mode dialog.
+     * Tests if clicking on the switch mode button shows the switch mode dialog.
      */
     @Test
     public void testSwitchModeButton() {
         onView(withId(R.id.switch_modes)).perform(click());
+        onView(withId(R.id.organizer_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.admin_button)).check(matches(isDisplayed()));
     }
 
     /**
@@ -38,14 +43,16 @@ public class MyEventActivityTest {
      */
     @Test
     public void testCameraButton() {
-        onView(withId(R.id.camera_button)).perform(click());
+        onView(withId(R.id.camera_button)).perform(click()).check(matches(isDisplayed()));
     }
 
     /**
-     * Tests if clicking on an event item navigates to AnnoucementActivity.
+     * Tests if clicking on an event item in the list navigates to AnnoucementActivity.
+     * Note: This assumes there are items in the list.
      */
     @Test
     public void testEventItemClick() {
         onView(withId(R.id.attendee_my_events_list)).perform(click());
+        onView(withId(R.id.attendee_my_events_list)).check(matches(isDisplayed()));
     }
 }
