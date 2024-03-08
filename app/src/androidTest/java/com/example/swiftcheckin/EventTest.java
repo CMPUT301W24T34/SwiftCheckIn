b3b8e149692a5210611478383aee0315e082941b
Kunaal Gupta <kunaalgupta@hotmail.com>
2024-03-07 21:09:01 -0700

    added event & EventViewAdapter Tests

--- app/src/androidTest/java/com/example/swiftcheckin/EventTest.java
+++ app/src/androidTest/java/com/example/swiftcheckin/EventTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertEquals;
 
 public class EventTest {
 
+    /**
+     * Test for getting the device ID.
+     */
     @Test
     public void testGetDeviceId() {
         Event event = new Event();
@@ -13,6 +16,9 @@ public class EventTest {
         assertEquals("TestDeviceId", event.getDeviceId());
     }
 
+    /**
+     * Test for getting the maximum number of attendees.
+     */
     @Test
     public void testGetMaxAttendees() {
         Event event = new Event();
@@ -20,5 +26,76 @@ public class EventTest {
         assertEquals(50, event.getMaxAttendees());
     }
 
-    // You can add more tests for other getters and setters in the Event class
+    /**
+     * Test for getting the event title.
+     */
+    @Test
+    public void testGetEventTitle() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("Test Event", event.getEventTitle());
+    }
+
+    /**
+     * Test for getting the location of the event.
+     */
+    @Test
+    public void testGetLocation() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("Location", event.getLocation());
+    }
+
+    /**
+     * Test for getting the description of the event.
+     */
+    @Test
+    public void testGetDescription() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("Description", event.getDescription());
+    }
+
+    /**
+     * Test for getting the URL of the event image.
+     */
+    @Test
+    public void testGetEventImageUrl() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("ImageUrl", event.getEventImageUrl());
+    }
+
+    /**
+     * Test for getting the start date of the event.
+     */
+    @Test
+    public void testGetStartDate() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("2024-03-01", event.getStartDate());
+    }
+
+    /**
+     * Test for getting the end date of the event.
+     */
+    @Test
+    public void testGetEndDate() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-02", "10:00", "12:00");
+        assertEquals("2024-03-02", event.getEndDate());
+    }
+
+    /**
+     * Test for getting the start time of the event.
+     */
+    @Test
+    public void testGetStartTime() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("10:00", event.getStartTime());
+    }
+
+    /**
+     * Test for getting the end time of the event.
+     */
+    @Test
+    public void testGetEndTime() {
+        Event event = new Event("Test Event", "Description", "Location", "DeviceId", "ImageUrl", "2024-03-01", "2024-03-01", "10:00", "12:00");
+        assertEquals("12:00", event.getEndTime());
+    }
+
 }
---
+++ app/src/androidTest/java/com/example/swiftcheckin/MyEventActivityTest.java
@@ -0,0 +1,52 @@
+package com.example.swiftcheckin;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * This class contains simple UI tests for MyEventActivity.
+ */
+public class MyEventActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MyEventActivity> scenarioRule = new ActivityScenarioRule<>(MyEventActivity.class);
+
+    /**
+     * Tests if the profile button click navigates to ProfileActivity.
+     */
+    @Test
+    public void testProfileButton() {
+        onView(withId(R.id.profile_picture)).perform(click());
+    }
+
+    /**
+     * Tests if the switch mode button click shows the switch mode dialog.
+     */
+    @Test
+    public void testSwitchModeButton() {
+        onView(withId(R.id.switch_modes)).perform(click());
+    }
+
+    /**
+     * Tests if clicking on the camera button navigates to QRCodeScannerActivity.
+     */
+    @Test
+    public void testCameraButton() {
+        onView(withId(R.id.camera_button)).perform(click());
+    }
+
+    /**
+     * Tests if clicking on an event item navigates to AnnoucementActivity.
+     */
+    @Test
+    public void testEventItemClick() {
+        // Assuming there's at least one event item in the list
+        onView(withId(R.id.attendee_my_events_list)).perform(click());
+    }
+}
