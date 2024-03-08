3601da5c0493d3557b0fda646510f010cb0b69d1
“Prateek <prateekannam@gmail.com>
2024-03-08 01:20:07 -0700

    Organizer Tests

--- app/src/androidTest/java/com/example/swiftcheckin/OrganizerTest.java
+++ app/src/androidTest/java/com/example/swiftcheckin/OrganizerTest.java
@@ -1,6 +1,7 @@
 package com.example.swiftcheckin;
 
 import static android.app.PendingIntent.getActivity;
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -9,6 +10,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.anything;
 
 import android.widget.ListView;
 
@@ -19,16 +22,20 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+/**
+ * This is a test to test the basic tasks of an Organizer, which is to add events and view who has signed up as of now.
+ */
 public class OrganizerTest {
 
     // What tests will I add?
-    // Need to see if I can access the organizer Activity.
+    // Need to see if I can access the organizer Activity
+
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new
-            ActivityScenarioRule<MainActivity>(MainActivity.class);
+            ActivityScenarioRule<MainActivity>(MainActivity.class);   // To keep consistent and start at main
 
-    /*
-    Switch activity
+    /**
+     * Test meant to see if the user is able to switch to organizer mode.
      */
     @Test
     public void organizerSwitch(){
@@ -37,11 +44,18 @@ public class OrganizerTest {
         onView(withId(R.id.organizer)).check(matches(isDisplayed()));
     }
 
+    /**
+     * Test to see if the user can add an event. This test in particular assumes that the user has created no events.
+     */
     @Test
     public void testAddEvent(){
+
         onView(withId(R.id.switch_modes)).perform(click());
         onView(withId(R.id.organizer_button)).perform(click());
         onView(withId(R.id.organizer)).check(matches(isDisplayed()));
+        // Citation: OpenAI, 03-07-2024, ChatGPT, How to get items in a listview
+        // output was onView(withId(R.id.list_view_id)).check(matches(hasChildCount(0)));
+        onView(withId(R.id.event_list)).check(matches(hasChildCount(0)));
         onView(withId(R.id.add_event_button)).perform(click());
         onView(withId(R.id.eventName)).perform(ViewActions.clearText(), ViewActions.typeText("Test 1"));
         onView(withId(R.id.eventPageAddressEditText)).perform(ViewActions.clearText(), ViewActions.typeText("10000 1000"));
@@ -50,6 +64,8 @@ public class OrganizerTest {
         onView(withId(R.id.eventAddActivity_eventEndDate_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("10182024"));
         onView(withId(R.id.eventAddActivity_eventEndTime_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("9:00"));
         onView(withId(R.id.editMaxAttendeeText)).perform(ViewActions.clearText(), ViewActions.typeText("9"));
+        // Citation: How to close the keyboard using espressso, Stack Overflow, License: CC-BY-SA, user name Tonny Tonny, "I can't make ViewActions.closeSoftKeyboard() work in Espresso 2.2.2", 2013-09-08,
+        // https://stackoverflow.com/questions/39580415/i-cant-make-viewactions-closesoftkeyboard-work-in-espresso-2-2-2
         onView(withId(R.id.editMaxAttendeeText)).perform(closeSoftKeyboard());
         onView(withId(R.id.eventPageDescriptionEditText)).perform(ViewActions.clearText(), ViewActions.typeText("This is to be tested."));
         onView(withId(R.id.eventPageDescriptionEditText)).perform(closeSoftKeyboard());
@@ -57,14 +73,29 @@ public class OrganizerTest {
         onView(withId(R.id.fragmentQrCodeMenu1NewButton)).perform(click());
         onView(withId(R.id.qrCodeSelectionSuccessLayout_saveButton)).perform(click());
         onView(withId(R.id.organizer)).check(matches(isDisplayed()));
-
+        onView(withId(R.id.event_list)).check(matches(hasChildCount(1)));
     }
 
-    // Need to see if I can add an event.
-    /*
-    See if smth pops up. Some sign
+    /**
+     * Tests to see if the user can view a sign up.
+     * By this point, an event has been added, so we can see if that activity has been opened.
      */
-    // Need to see if I can view the event.
+    @Test
+    public void testViewSignup(){
+        onView(withId(R.id.switch_modes)).perform(click());
+        onView(withId(R.id.organizer_button)).perform(click());
+        onView(withId(R.id.organizer)).check(matches(isDisplayed()));
 
+        // Citation: OpenAI, 03-07-2024, ChatGPT, How to click on a listview item
+        // output onData(anything())
+        //                .inAdapterView(withId(R.id.your_list_view_id))
+        //                .atPosition(0)
+        //                .perform(click());
+        onData(anything())
+                .inAdapterView(withId(R.id.event_list))
+                .atPosition(0)
+                .perform(click());
+        onView(withId(R.id.viewAttendeeBackButton)).check(matches(isDisplayed()));
 
+    }
 }
