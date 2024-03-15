9930ab9983a4c98aa26078b24337d21b8bf06671
“Prateek <prateekannam@gmail.com>
2024-03-15 16:06:24 -0600

    organizer folder

--- app/src/main/java/com/example/swiftcheckin/admin/AdminEventArrayAdapter.java
+++ app/src/main/java/com/example/swiftcheckin/admin/AdminEventArrayAdapter.java
@@ -10,13 +10,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-<<<<<<< HEAD:app/src/main/java/com/example/swiftcheckin/AdminEventArrayAdapter.java
 import com.example.swiftcheckin.organizer.Event;
-=======
 import com.bumptech.glide.Glide;
-import com.example.swiftcheckin.Event;
 import com.example.swiftcheckin.R;
->>>>>>> 58b68cadbaed11e745ce58875c8f7bebd55a5ed7:app/src/main/java/com/example/swiftcheckin/admin/AdminEventArrayAdapter.java
 
 import java.util.ArrayList;
 /**
