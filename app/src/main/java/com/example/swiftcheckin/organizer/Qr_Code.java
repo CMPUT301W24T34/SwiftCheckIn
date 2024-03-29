4e357932321aa4738c4bc2cba570f9a32899509f
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-03-28 22:19:49 -0600

    update QR class
    new setters and getters

--- app/src/main/java/com/example/swiftcheckin/organizer/Qr_Code.java
+++ app/src/main/java/com/example/swiftcheckin/organizer/Qr_Code.java
@@ -1,2 +1,40 @@
-package com.example.swiftcheckin.organizer;public class Qr_Code {
+package com.example.swiftcheckin.organizer;
+
+import android.graphics.Bitmap;
+
+public class Qr_Code {
+    private Bitmap image;
+    private String eventID;
+    private String QrID;
+    private Boolean isPromo;
+
+    public String getEventID()
+    {
+        return this.eventID;
+    }
+
+    public void setEventID(String event)
+    {
+        this.eventID = event;
+    }
+
+    public String getQrID()
+    {
+        return this.QrID;
+    }
+
+    public void setQrID(String QrID)
+    {
+        this.QrID = QrID;
+    }
+
+    public Bitmap getImage()
+    {
+        return this.image;
+    }
+
+    public void setImage(Bitmap img)
+    {
+        this.image = img;
+    }
 }
