e5719ef5a54496202dfdd8d7e6207ce87e490148
Alyse Ridge <94921692+aridge03@users.noreply.github.com>
2024-04-06 17:44:25 -0600

    Add tests

--- app/src/androidTest/java/com/example/swiftcheckin/AdminTest.java
+++ app/src/androidTest/java/com/example/swiftcheckin/AdminTest.java
@@ -1,4 +1,5 @@
 package com.example.swiftcheckin;
+import androidx.test.espresso.IdlingResource;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -14,6 +15,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -37,6 +39,7 @@ import android.widget.ListView;
 
 import com.example.swiftcheckin.admin.AdminActivity;
 import com.example.swiftcheckin.attendee.MainActivity;
+import com.example.swiftcheckin.attendee.Profile;
 import com.example.swiftcheckin.organizer.Event;
 
 import java.util.List;
@@ -60,23 +63,28 @@ public class AdminTest {
     @Test
     public void testDeleteEvent(){
         onView(withId(R.id.switch_modes)).perform(click());
-//        onView(withId(R.id.organizer_button)).perform(click());
-//        onView(withId(R.id.add_event_button)).perform(click());
-//        onView(withId(R.id.eventName)).perform(ViewActions.clearText(), ViewActions.typeText("Delete Event Test"));
-//        onView(withId(R.id.eventPageAddressEditText)).perform(ViewActions.clearText(), ViewActions.typeText("10000 1000"));
-//        onView(withId(R.id.eventAddActivity_StartDate_EditText)).perform(ViewActions.clearText(), replaceText("Apr 5 2024"));
-//        onView(withId(R.id.eventAddActivity_eventStartTime_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("7:00"));
-//        onView(withId(R.id.eventAddActivity_eventEndDate_EditText)).perform(ViewActions.clearText(),replaceText("Apr 5 2024"));
-//        onView(withId(R.id.eventAddActivity_eventEndTime_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("9:00"));
-//        onView(withId(R.id.editMaxAttendeeText)).perform(ViewActions.clearText(), ViewActions.typeText("9"));
-//        onView(withId(R.id.editMaxAttendeeText)).perform(closeSoftKeyboard());
-//        onView(withId(R.id.eventPageDescriptionEditText)).perform(ViewActions.clearText(), ViewActions.typeText("This is to be tested."));
-//        onView(withId(R.id.eventPageDescriptionEditText)).perform(closeSoftKeyboard());
-//        onView(withId(R.id.eventPageSaveButton)).perform(click());
-//        onView(withId(R.id.fragmentQrCodeMenu1NewButton)).perform(click());
-//        onView(withId(R.id.qrCodeSelectionSuccessLayout_saveButton)).perform(click());
-//        pressBack();
+        onView(withId(R.id.organizer_button)).perform(click());
+        onView(withId(R.id.add_event_button)).perform(click());
+        onView(withId(R.id.eventName)).perform(ViewActions.clearText(), ViewActions.typeText("Delete Event Test"));
+        onView(withId(R.id.eventPageAddressEditText)).perform(ViewActions.clearText(), ViewActions.typeText("10000 1000"));
+        onView(withId(R.id.eventAddActivity_StartDate_EditText)).perform(ViewActions.clearText(), replaceText("Apr 5 2024"));
+        onView(withId(R.id.eventAddActivity_eventStartTime_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("7:00"));
+        onView(withId(R.id.eventAddActivity_eventEndDate_EditText)).perform(ViewActions.clearText(),replaceText("Apr 5 2024"));
+        onView(withId(R.id.eventAddActivity_eventEndTime_EditText)).perform(ViewActions.clearText(), ViewActions.typeText("9:00"));
+        onView(withId(R.id.editMaxAttendeeText)).perform(ViewActions.clearText(), ViewActions.typeText("9"));
+        onView(withId(R.id.editMaxAttendeeText)).perform(closeSoftKeyboard());
+        onView(withId(R.id.eventPageDescriptionEditText)).perform(ViewActions.clearText(), ViewActions.typeText("This is to be tested."));
+        onView(withId(R.id.eventPageDescriptionEditText)).perform(closeSoftKeyboard());
+        onView(withId(R.id.eventPageSaveButton)).perform(click());
+        onView(withId(R.id.fragmentQrCodeMenu1NewButton)).perform(click());
+        onView(withId(R.id.qrCodeSelectionSuccessLayout_saveButton)).perform(click());
+        pressBack();
         onView(withId(R.id.admin_button)).perform(click());
+
+        onView(withId(R.id.editTextTextPassword)).perform(ViewActions.clearText(), ViewActions.typeText("SwiftCheckIn"));
+        onView(withId(R.id.login_button )).perform(click());
+
+
         ActivityScenario<AdminActivity> activityScenario = ActivityScenario.launch(AdminActivity.class);
         activityScenario.onActivity(activity -> {
             List<Event> eventList = activity.getEventList(); assertNotNull(eventList);
@@ -108,31 +116,122 @@ public class AdminTest {
                 .check(matches(withText("Delete Event Test")))
                 .perform(click());
 
-        onView(withId(R.id.remove_tab_button));
+        onView(withId(R.id.remove_tab_button))
+                .check(matches(isDisplayed())) // Check if the button is visible
+                .perform(click()); // Try clicking the button
+
+// Optionally, you can introduce a delay before clicking
+        try {
+            Thread.sleep(5000); // 1 second delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+// Retry clicking the button
+        onView(withId(R.id.remove_tab_button)).perform(click());
+
+
+
+
+ activityScenario.onActivity(activity -> {
+List<Event> eventList = activity.getEventList(); assertNotNull(eventList);
+//
+//            // Assert that eventList contains the expected event
+boolean eventFound = false;
+for (Event event : eventList) {
+if (event.getEventTitle().equals("Delete Event Test")) { // Assuming 'getName()' returns the event name
+//                    // Event found, set flag to true
+eventFound = true;
+          break;
+             }
+            }
+//            // Assert that the event was found in the list
+           assertFalse("Expected event found in the event list", eventFound);
+        });
+
+    }
+    @Test
+    public void testDeleteProfile(){
+        onView(withId(R.id.profile_picture)).perform(click());
+        onView(withId(R.id.profile)).check(matches(isDisplayed()));
+        onView(withId(R.id.settings_button)).perform(click());
+        onView(withId(R.id.settings)).check(matches(isDisplayed()));
+        onView(withId(R.id.name)).perform(ViewActions.clearText(), ViewActions.typeText("DeleteProfileTest"));
+        onView(withId(R.id.word_name)).perform(click());
+        onView(withId(R.id.save_button)).perform(click());
+
+        onView(withId(R.id.admin_button)).perform(click());
+
+        onView(withId(R.id.editTextTextPassword)).perform(ViewActions.clearText(), ViewActions.typeText("SwiftCheckIn"));
+        onView(withId(R.id.login_button )).perform(click());
+        onView(withId(R.id.profile_button)).perform(click());
+
+
+        ActivityScenario<AdminActivity> activityScenario = ActivityScenario.launch(AdminActivity.class);
+        activityScenario.onActivity(activity -> {
+            List<Profile> profileList = activity.getProfileList(); assertNotNull(profileList);
+
+            // Assert that eventList contains the expected event
+            i = 0;
+            boolean profileFound = false;
+            for (Profile profile : profileList) {
+                if (profile.getName().equals("DeleteProfileTest")) { // Assuming 'getName()' returns the event name
+                    // Event found, set flag to true
+                    profileFound = true;
+                    break;
+                }
+                i++;
+            }
+            // Assert that the event was found in the list
+            assertTrue("Expected profile not found in the event list", profileFound);
+        });
+
+        // call the event list from admin activity and check to see if the event "Delete Event Test"
+        // then get the index of the event
+        // click the list at that index
+        //then click remove_button
+        //assert that the event is no longer in event list
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(i) // Replace 0 with the position of the item if it's not the first one
+                .onChildView(withId(R.id.name_text))
+                .check(matches(withText("DeleteProfileTest")))
+                .perform(click());
+
+        onView(withId(R.id.remove_tab_button))
+                .check(matches(isDisplayed())) // Check if the button is visible
+                .perform(click()); // Try clicking the button
+
+// Optionally, you can introduce a delay before clicking
+        try {
+            Thread.sleep(5000); // 1 second delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+// Retry clicking the button
         onView(withId(R.id.remove_tab_button)).perform(click());
 
 
 
 
         activityScenario.onActivity(activity -> {
-            List<Event> eventList = activity.getEventList(); assertNotNull(eventList);
-
-            // Assert that eventList contains the expected event
-            boolean eventFound = false;
-            for (Event event : eventList) {
-                if (event.getEventTitle().equals("Delete Event Test")) { // Assuming 'getName()' returns the event name
+            List<Profile> profileList = activity.getProfileList(); assertNotNull(profileList);
+//
+//            // Assert that eventList contains the expected event
+            boolean profileFound = false;
+            for (Profile profile : profileList) {
+                if (profile.getName().equals("DeleteProfileTest")) { // Assuming 'getName()' returns the event name
                     // Event found, set flag to true
-                    eventFound = true;
+                    profileFound = true;
                     break;
                 }
             }
-            // Assert that the event was found in the list
-            assertFalse("Expected event found in the event list", eventFound);
+//            // Assert that the event was found in the list
+            assertFalse("Expected event found in the event list",profileFound);
         });
 
     }
-
-
 
 }
 
--- app/src/main/java/com/example/swiftcheckin/admin/AdminActivity.java
+++ app/src/main/java/com/example/swiftcheckin/admin/AdminActivity.java
@@ -295,6 +295,9 @@ public class AdminActivity extends AppCompatActivity {
     public List<Event> getEventList() {
         return eventList;
     }
+    public List<Profile> getProfileList() {
+        return profileList;
+    }
 
 
 
