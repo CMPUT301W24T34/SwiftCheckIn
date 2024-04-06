e53ed5006d084f3d58981425b75d6f22fc53586f
Alyse Ridge <94921692+aridge03@users.noreply.github.com>
2024-04-05 21:29:44 -0600

    Add password functionality

---
+++ app/src/main/java/com/example/swiftcheckin/attendee/AdminPassword.java
@@ -0,0 +1,54 @@
+package com.example.swiftcheckin.attendee;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.swiftcheckin.R;
+import com.example.swiftcheckin.admin.AdminActivity;
+
+public class AdminPassword extends AppCompatActivity {
+
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+        TextView backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = passwordEditText.getText().toString().trim();
+
+                if (password.equalsIgnoreCase("SwiftCheckIn")) {
+                    Toast.makeText(AdminPassword.this, "Switched to admin", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AdminPassword.this, AdminActivity.class);
+                    startActivity(intent);
+                    finish(); // Finish the login activity after starting AdminActivity
+                } else {
+                    Toast.makeText(AdminPassword.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
