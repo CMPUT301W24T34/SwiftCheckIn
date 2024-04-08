14a16c3b1fd8398d999714083a2db79a29f5510a
Pooja <pooja_bhambri@hotmail.com>
2024-04-08 09:57:39 -0600

    Add more citations

--- app/src/main/java/com/example/swiftcheckin/attendee/SettingsActivity.java
+++ app/src/main/java/com/example/swiftcheckin/attendee/SettingsActivity.java
@@ -107,7 +107,7 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         // Citation: OpenAI, 04-07-2024, ChatGPT, How to change view to front of text when focus is gone
-        // output is functions below
+        // output is functions below, the setOnFocusChangeListener and onFocusChange method
         emailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -185,8 +185,9 @@ public class SettingsActivity extends AppCompatActivity {
         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 
-    // Citation: OpenAI, 03-05-2024, ChatGPT, Checking if phone string matches pattern
-    // gave me output of this "\\d{2}/\\d{2}/\\d{4}" and told me to use Pattern.matches(pattern, phone);
+    // Citation: OpenAI, 03-05-2024, ChatGPT, Checking if string matches pattern
+    // gave me output of this "\\d{2}/\\d{2}/\\d{4}" and told me to use Pattern.matches(pattern, birthday);
+    // (birthday has been changed to phone for this method)
     /**
      * This checks if the inputted phone number is of valid format
      * @param phone - string to check
@@ -264,6 +265,7 @@ public class SettingsActivity extends AppCompatActivity {
         if (locationPermission) {
             data.put("locationPermission", "True");
             fb.saveProfileToFirebase(deviceId, data);
+            // Citation: OpenAI, 04-06-2024, ChatGPT, How to get context to use in this call
             locationReceiver.getLocation(deviceId, getApplicationContext());
 
         }
